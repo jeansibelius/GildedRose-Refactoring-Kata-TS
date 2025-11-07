4bdae12e68999b9e80035bddef37420a93d70a20
Rafael Linnankoski <rafael.linnankoski@krakenflex.com>
2025-11-07 13:10:22 +0900

    chore: simplify test setup

--- TypeScript/test/approvals.spec.ts
+++ TypeScript/test/approvals.spec.ts
@@ -3,6 +3,16 @@ import { describe, it, expect } from "vitest";
 
 import { Item, GildedRose } from "@/gilded-rose";
 
+/**
+ * This test uses Vitest Snapshot, similar to [Jest Snapshot](https://goo.gl/fbAQLP).
+ *
+ * There are two test cases here with different styles:
+ * <li>"foo" is more similar to the unit test from the 'Java' version
+ * <li>"thirtyDays" is more similar to the TextTest from the 'Java' version
+ *
+ * I suggest choosing one style to develop and deleting the other.
+ */
+
 describe("Gilded Rose Approval", () => {
   it("should foo", () => {
     const gildedRose = new GildedRose([new Item("foo", 0, 0)]);
